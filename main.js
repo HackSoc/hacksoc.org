e091480c48444cc8f3ab474a24d07bf7f22058f8
Luke Moll <luke.moll99@gmail.com>
2018-06-14 23:18:44 +0100

    Added minutes page generation

--- main.js
+++ main.js
@@ -11,6 +11,13 @@ fse.emptyDirSync(outDir);
 
 compileTemplate = template => Handlebars.compile(template.toString('UTF-8'));
 
+/**
+ * 
+ * @param {string} dirname The directory to read HTML content from
+ * @param {Handlebars.TemplateDelegate} wrapperTemplate Handlebars template to apply to the content
+ * @param {Object} globalContext Context to use for every page
+ * @returns {Promise} no resolve value, wraps around a Promise.all of all of the files
+ */
 function regularDir(dirname, wrapperTemplate, globalContext) {
     return fse.readdir(dirname).then(listing => {
         /**
@@ -29,17 +36,50 @@ function regularDir(dirname, wrapperTemplate, globalContext) {
                     let matter = docmatter(content.toString('UTF-8'));
 
                     return wrapperTemplate(Object.assign({ // Merge this object with the properties in context.yaml
-                        body: matter.body,
-                        title: filename.replace(/\.html$/i,'') // TODO title from YAML
+                        body: matter.body
                     }, globalContext, yaml.safeLoad(matter.header)))
                 })
                 .then(html => fse.writeFile(path.join(outDir, filename), html))
             );
         return Promise.all(promises)
-
     })
 }
 
+/**
+ * Generates an index of a directories PDF files, and copies them to the HTML folder.
+ * @param {string} dirname Directory to find .PDF files in
+ * @param {string} filename Filename of this page
+ * @param {Handlebars.TemplateDelegate} wrapperTemplate Handlebars template to apply to the whole page
+ * @param {Handlebars.TemplateDelegate} minuteTemplate Template to use on the listing
+ * @param {Object} globalContext Context to use for every page
+ */
+function minutes(dirname, filename, wrapperTemplate, minuteTemplate, globalContext) {
+    return fse.readdir(dirname).then(listing => {
+        let minutes = listing.filter(l => /\.pdf$/.test(l))
+                        .map(fn => {
+                            let m = /^(\d{4}-\d\d-\d\d)-([a-z0-9 ]+)\.pdf$/i.exec(fn);
+                            return {
+                                url: path.join(filename, fn),
+                                meeting: m[2],
+                                date: m[1],
+                                date_t: new Date(m[1])
+                            }
+                        }).sort((f1, f2) => Math.sign(f1.date_t - f2.date_t));
+                        // Has the potential to have meeting on the same date in the wrong order
+                        // Cross that bridge when etc
+
+        fse.writeFile(path.join(outDir, filename), 
+            wrapperTemplate(Object.assign({
+                body: minuteTemplate(minutes),
+                title: "Minutes"
+            }, globalContext))
+        ).then(fse.copy(dirname, path.join(outDir, dirname)))
+    })
+}
+
+/**
+ * @returns {Promise<Object>} Resolves to the global context object
+ */
 function getGlobalContext() {
     return fse.readFile('templates/context.yaml')
         .then(contents => {
@@ -50,17 +90,34 @@ function getGlobalContext() {
         })
 }
 
-getGlobalContext().then(globalContext => {
-    
-    Promise.resolve(
-        fse.readFile('templates/wrapper.handlebars') // Read the template
-            .then(compileTemplate) // compile it
-            .then(wrapperTemplate => regularDir('regular', wrapperTemplate,globalContext)) // pass it to the Promise to apply it to all in regular/
-    ).then(() => {console.log(`Writen regular files!`)})
-    .catch(console.error);
-    
-    fse.copySync('static', path.join(outDir, 'static'))
-    console.log(`Copied static/ directory!`)
+// Main stuff here.       
+let p_mkOutDir = fse.mkdirp(outDir)
+    .then(() => fse.emptyDir(outDir));
 
-})
+let p_copyStatic = fse.copy('static', path.join(outDir, 'static'))
+    .then(console.log("Copied static files!"));
+    
+    p_mkOutDir.then(() => p_copyStatic);
 
+    
+let p_contextAndTemplates = Promise.all(
+    [
+        getGlobalContext(),
+        fse.readFile('templates/wrapper.handlebars').then(compileTemplate),
+        fse.readFile('templates/minutes.handlebars').then(compileTemplate)
+    ])
+    .then(results => ({
+        globalContext: results[0],
+        wrapperTemplate: results[1],
+        minutesTemplate: results[2]
+    }))
+
+    
+    p_contextAndTemplates.then(obj => regularDir('regular',obj.wrapperTemplate, obj.globalContext))
+        .then(()=>console.log("Written regular files!"))
+        .catch(console.log);
+    p_contextAndTemplates.then(obj => minutes('minutes', 'minutes.html', obj.wrapperTemplate, obj.minutesTemplate, obj.globalContext))
+        .then(()=>console.log("Written minutes files!"))
+        .catch(console.log);
+
+p_mkOutDir.then(() => p_contextAndTemplates);
