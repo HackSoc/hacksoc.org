b310226036fcc3e92ec02a5b6a8f963d3ebcae6d
Aaron Christiansen <aaronc20000@gmail.com>
2019-10-13 21:28:30 +0100

    Tidy height calculation; add row count prediction on load

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -10,6 +10,45 @@ function isDateStringToday(date) {
 }
 
 /**
+ * Calculates the number of rows required for the HTML calendar.
+ * @param {Number} year The year for the calendar.
+ * @param {Number} month The month for the calendar, between 1 and 12.
+ * @returns {Number} The number of rows required.
+ */
+function rowsForCalendar(year, month) {
+    let currentDate = new Date(year, month - 1, 1);
+
+    // If it's Monday, the first row will be added by the loop
+    var result = currentDate.getDay() == 1 ? 0 : 1;
+
+    while (currentDate.getMonth() + 1 == month) {
+        if (currentDate.getDay() == 1) {
+            result++;
+        }
+
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    return result;
+}
+
+/**
+ * Gets the required height of a given row.
+ * @param {Number} index The index of this row, starting from 0, excluding the header.
+ * @param {*} rows The total number of rows, excluding the header.
+ * @returns {Number} The height of the row in pixels.
+ */
+function calendarRowHeight(index, rows) {
+    // Due to Cursed Stuff, presumably borders, heights differ by a few pixels
+    // between different numbers of rows.
+    return {
+        4: [123, 121, 121, 121],
+        5: [98, 97, 97, 97, 97],
+        6: [51, 50, 50, 50, 50, 50]
+    }[rows][index];
+}
+
+/**
  * Reloads the HTML calendar.
  * @param {Number} year The year for the calendar.
  * @param {Number} month The month for the calendar, between 1 and 12.
@@ -18,8 +57,27 @@ async function loadCalendar(year, month) {
     closeInfoPanel();
     coverCalendar();
 
-    // Write data attributes for next/previous month
     const calendarElement = document.getElementById('calendar');
+    const tableRows = document.querySelectorAll('#calendar tbody tr');
+    
+    // If this is the first load, predict the number of rows
+    if (!calendarElement.dataset.month) {
+        const predictedNumberOfRows = rowsForCalendar(year, month);
+        for (let i = 0; i < 6; i++) {
+            const tableRow = tableRows[i];
+
+            if (i < predictedNumberOfRows) {
+                tableRow.classList.remove('hidden');
+                
+                let thisRowHeight = calendarRowHeight(i, predictedNumberOfRows);
+                tableRow.setAttribute("style", `height: ${thisRowHeight}px`);
+            } else {
+                tableRow.classList.add('hidden');
+            }
+        }
+    }
+
+    // Write data attributes for next/previous month
     calendarElement.dataset.month = month;
     calendarElement.dataset.year = year;
 
@@ -45,16 +103,8 @@ async function loadCalendar(year, month) {
     const calendar = await calendarJson.json();
 
     // Calculate heights
-    // It's too hard to do this mathematically because of border collapsing, so
-    // just use a table
     const numberOfRows = calendar.filter(x => x).length;
-    const eachRowHeight = {
-        4: 121,
-        5: 97,
-        6: 50
-    }[numberOfRows];
 
-    const tableRows = document.querySelectorAll('#calendar tbody tr');
     for (let iRow = 0; iRow < 6; iRow++) {
         const tableRow = tableRows[iRow];
         const dataRow = calendar[iRow];
@@ -68,13 +118,8 @@ async function loadCalendar(year, month) {
             continue;
         }
 
-        // More cursed height calculation to fix heights being one or two by
-        // making the first row slightly taller
-        let thisRowHeight = eachRowHeight;
-        if (iRow == 0) {
-            thisRowHeight++;
-            if (numberOfRows == 4) thisRowHeight++;
-        }
+        // Fixed height is used for each row
+        let thisRowHeight = calendarRowHeight(iRow, numberOfRows);
         tableRow.setAttribute("style", `height: ${thisRowHeight}px`);
 
         for (let iCol = 0; iCol < 7; iCol++) {
