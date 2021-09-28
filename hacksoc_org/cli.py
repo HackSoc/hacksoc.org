d87d30e404031701141535a1f3772266831d272b
Luke Moll <lukemoll@users.noreply.github.com>
2021-09-28 12:42:12 +0100

    Added command-line help strings

--- hacksoc_org/cli.py
+++ hacksoc_org/cli.py
@@ -18,12 +18,42 @@ def subcommand(command_str: str) -> Callable[[Callable], Callable]:
 
 
 def main(args=None):
-    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser = argparse.ArgumentParser(
+        allow_abbrev=False,
+        epilog="""
+SUBCOMMANDS
 
-    parser.add_argument("action", choices=subcommand_handlers.keys())
-    # TODO: add help to prelude/preamble
+  run
+    Starts a local development server on https://localhost:5000/. Automatically
+    reloads when templates or Python code is changed. Recommended while 
+    developing pages or features.
 
-    parser.add_argument("--markdown", choices=["markdown2", "cmark"], default="markdown2")
+  freeze
+    Saves all URL routes to HTML files and copies `static/` to the `build/` 
+    directory. The resulting directory can be used with any standard HTTP
+    server (nginx, Apache, etc). In development, recommend using `serve` 
+    instead for convenience.
+
+  serve
+    Calls `freeze` then starts a local HTTP server from `build/` on 
+    https://localhost:5000/. Will not automatically rebuild the website on
+    content change, you will need to re-run `serve`. Recommended to use this at
+    least once to check that a) new content is part of the "frozen" site and b)
+    no errors occur in freezing the site.
+""".strip(),
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument(
+        "action", choices=subcommand_handlers.keys(), help="Subcommand to run (see below)"
+    )
+
+    parser.add_argument(
+        "--markdown",
+        choices=["markdown2", "cmark"],
+        default="markdown2",
+        help="Markdown backend to use (default markdown2)",
+    )
 
     args = parser.parse_args(args=args)
 
