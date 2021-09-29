6ce95ae0a7ee960af3c90b7e434dcb600218b3e8
Luke Moll <lukemoll@users.noreply.github.com>
2021-09-29 11:45:16 +0100

    Fix live-reloading after hacksoc_org migration

--- hacksoc_org/__init__.py
+++ hacksoc_org/__init__.py
@@ -14,6 +14,9 @@ from os import path
 app = flask.Flask(__name__, static_folder=None, template_folder=None)
 # these folders are defined in the Blueprint anyway
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["ENV"] = "development"
+app.config["DEBUG"] = True
+
 
 app.jinja_env.add_extension("jinja2.ext.do")
 # adds support for the jinja "do" statement
